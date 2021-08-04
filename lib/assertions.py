bbe6e725de231a1fe6d943607714e73e88bc1d02
NataliaSinitsyna <natalia.a.sinitsyna@gmail.com>
2021-08-04 08:21:17 +0300

    new tasks

---
+++ lib/assertions.py
@@ -0,0 +1,13 @@
+from requests import Response
+import json
+
+class Assertions:
+    @staticmethod #статический метод, так как этот класс не является прямым наследником для наших тестов
+    def assert_json_value_name(response: Response, name, expected_value, error_message):
+        try:
+            response_as_dict = response.json()
+        except json.JSONDecodeError:
+            assert False, f"Response is not in JSON format. Response text is '{response.text}'"
+
+        assert name in response_as_dict, f"Response JSON doesn't have key '{name}'"
+        assert response_as_dict[name] == expected_value, error_message
